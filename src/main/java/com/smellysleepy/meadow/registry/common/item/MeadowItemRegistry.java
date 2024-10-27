ac2cb034a88248bf3b937bf4494e576beca61313
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-10-27 19:33:01 +0100

    Update MeadowItemRegistry.java

--- src/main/java/com/smellysleepy/meadow/registry/common/item/MeadowItemRegistry.java
+++ src/main/java/com/smellysleepy/meadow/registry/common/item/MeadowItemRegistry.java
@@ -91,10 +91,10 @@ public class MeadowItemRegistry {
     public static final RegistryObject<Item> ROCKY_PEARLFLOWER = register("rocky_pearlflower", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.PEARLFLOWER.get(), p));
 
     public static final RegistryObject<Item> AUREATE_WHEAT_CROP = register("aureate_wheat_test", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.AUREATE_WHEAT_CROP.get(), p));
-    public static final RegistryObject<Item> MEADOW_GRASS_BLOCK = register("meadow_grass_block", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.MEADOW_GRASS_BLOCK.get(), p));
-    public static final RegistryObject<Item> TALL_MEADOW_GRASS = register("tall_meadow_grass", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.TALL_MEADOW_GRASS.get(), p));
-    public static final RegistryObject<Item> MEDIUM_MEADOW_GRASS = register("medium_meadow_grass", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.MEDIUM_MEADOW_GRASS.get(), p));
     public static final RegistryObject<Item> SHORT_MEADOW_GRASS = register("short_meadow_grass", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.SHORT_MEADOW_GRASS.get(), p));
+    public static final RegistryObject<Item> MEDIUM_MEADOW_GRASS = register("medium_meadow_grass", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.MEDIUM_MEADOW_GRASS.get(), p));
+    public static final RegistryObject<Item> TALL_MEADOW_GRASS = register("tall_meadow_grass", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.TALL_MEADOW_GRASS.get(), p));
+    public static final RegistryObject<Item> MEADOW_GRASS_BLOCK = register("meadow_grass_block", DEFAULT_PROPERTIES(), (p) -> new BlockItem(MeadowBlockRegistry.MEADOW_GRASS_BLOCK.get(), p));
 
 
     public static <T extends Item> RegistryObject<T> register(String name, Item.Properties properties, Function<Item.Properties, T> function) {
