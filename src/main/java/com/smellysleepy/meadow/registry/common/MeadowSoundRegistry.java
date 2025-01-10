e43fd4417101ec6b1c1a06d1dbcca13d4fb64bfe
SammySemicolon <malummoddevelopment@gmail.com>
2025-01-11 00:42:17 +0100

    Update MeadowSoundRegistry.java

--- src/main/java/com/smellysleepy/meadow/registry/common/MeadowSoundRegistry.java
+++ src/main/java/com/smellysleepy/meadow/registry/common/MeadowSoundRegistry.java
@@ -18,11 +18,11 @@ public class MeadowSoundRegistry {
 
     public static RegistryObject<SoundEvent> CALCIFICATION_MOTIF = register(SoundEvent.createVariableRangeEvent(MeadowMod.meadowModPath("calcification_motif")));
 
-    public static final SoundType CALCIFIED_ROCK = new CalcifiedSoundType(1.0F, 0.8f, ()->SoundEvents.DEEPSLATE_BREAK, ()->SoundEvents.NETHERRACK_STEP, ()->SoundEvents.NETHERRACK_PLACE, ()->SoundEvents.NETHERRACK_HIT, ()->SoundEvents.NETHERRACK_FALL);
-    public static final SoundType CALCIFIED_BRICK = new CalcifiedSoundType(1.0F, 0.8f, ()->SoundEvents.DEEPSLATE_TILES_BREAK, ()->SoundEvents.DEEPSLATE_TILES_STEP, ()->SoundEvents.DEEPSLATE_TILES_PLACE, ()->SoundEvents.DEEPSLATE_TILES_HIT, ()->SoundEvents.DEEPSLATE_TILES_FALL);
-    public static final SoundType HEAVY_CALCIFIED_BRICK = new CalcifiedSoundType(1.0F, 0.6f, ()->SoundEvents.POLISHED_DEEPSLATE_BREAK, ()->SoundEvents.POLISHED_DEEPSLATE_STEP, ()->SoundEvents.POLISHED_DEEPSLATE_PLACE, ()->SoundEvents.POLISHED_DEEPSLATE_HIT, ()->SoundEvents.POLISHED_DEEPSLATE_FALL);
-    public static final SoundType CALCIFIED_COVERING = new CalcifiedSoundType(1.0F, 1.1f, ()->SoundEvents.GRASS_BREAK, ()->SoundEvents.VINE_STEP, ()->SoundEvents.GRASS_PLACE, ()->SoundEvents.GRASS_HIT, ()->SoundEvents.GRASS_FALL);
-    public static final SoundType CALCIFIED_WOOD = new CalcifiedSoundType(1.0F, 0.8f, ()->SoundEvents.CHERRY_WOOD_BREAK, ()->SoundEvents.CHERRY_WOOD_STEP, ()->SoundEvents.CHERRY_WOOD_PLACE, ()->SoundEvents.CHERRY_WOOD_HIT, ()->SoundEvents.CHERRY_WOOD_FALL);
+    public static final SoundType CALCIFIED_ROCK = new CalcifiedSoundType(1f, 0.8f, ()->SoundEvents.DEEPSLATE_BREAK, ()->SoundEvents.NETHERRACK_STEP, ()->SoundEvents.NETHERRACK_PLACE, ()->SoundEvents.NETHERRACK_HIT, ()->SoundEvents.NETHERRACK_FALL);
+    public static final SoundType CALCIFIED_BRICK = new CalcifiedSoundType(1f, 0.8f, ()->SoundEvents.DEEPSLATE_TILES_BREAK, ()->SoundEvents.DEEPSLATE_TILES_STEP, ()->SoundEvents.DEEPSLATE_TILES_PLACE, ()->SoundEvents.DEEPSLATE_TILES_HIT, ()->SoundEvents.DEEPSLATE_TILES_FALL);
+    public static final SoundType HEAVY_CALCIFIED_BRICK = new CalcifiedSoundType(1f, 0.7f, ()->SoundEvents.NETHER_BRICKS_BREAK, ()->SoundEvents.NETHER_BRICKS_STEP, ()->SoundEvents.NETHER_BRICKS_PLACE, ()->SoundEvents.NETHER_BRICKS_HIT, ()->SoundEvents.NETHER_BRICKS_FALL);
+    public static final SoundType CALCIFIED_COVERING = new CalcifiedSoundType(2f, 1.1f, ()->SoundEvents.GRASS_BREAK, ()->SoundEvents.VINE_STEP, ()->SoundEvents.GRASS_PLACE, ()->SoundEvents.GRASS_HIT, ()->SoundEvents.GRASS_FALL);
+    public static final SoundType CALCIFIED_WOOD = new CalcifiedSoundType(1f, 0.8f, ()->SoundEvents.CHERRY_WOOD_BREAK, ()->SoundEvents.CHERRY_WOOD_STEP, ()->SoundEvents.CHERRY_WOOD_PLACE, ()->SoundEvents.CHERRY_WOOD_HIT, ()->SoundEvents.CHERRY_WOOD_FALL);
 
 
     public static RegistryObject<SoundEvent> register(SoundEvent soundEvent) {
